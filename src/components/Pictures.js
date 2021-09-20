0461758a70e233166b4dae7791c114a9c7092f88
AmilMasic <amil.masic@gmail.com>
2021-09-20 15:06:20 -0600

    fetching data from NASA API successfully

--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
+const apiKey = process.env.REACT_APP_NASA_API_KEY;
 
 class Pictures extends React.Component {
 
-  componentDidMount(){
+  constructor(){
+    super();
+    this.state = {
+      data: []
+    }
+  }
 
+  componentDidMount(){
+    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-7-27&api_key=${apiKey}`)
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Error fetching the pictures")
+        }
+        return response.json()
+      .then(data => {
+        this.setState({data: data});
+      })
+      .catch(err => {
+        throw Error(err.message);
+      });
+    });
   }
 
   render(){
