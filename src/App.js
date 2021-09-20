c617ad6040f7378f5b13b289c256d39c54a45b0e
AmilMasic <amil.masic@gmail.com>
2021-09-20 14:21:28 -0600

    boiler plate for Pictures component and connected it to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Pictures from './components/Pictures';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>"Spacestragram"</h1>
+      <Pictures/>
     </div>
   );
 }
---
+++ src/components/Pictures.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class Pictures extends React.Component {
+
+  componentDidMount(){
+
+  }
+
+  render(){
+    return (
+      <div>
+        <p>"This is the Pictures component"</p>
+      </div>
+    )
+  }
+}
+
+export default Pictures;
