737743e756762ba08258cd72fb3c2d47b7b62837
AmilMasic <amil.masic@gmail.com>
2021-09-21 16:13:31 +0000

    Deploying to gh-pages from  @ 21e89e6059ad792bf9389d704db68ed3efeaf057 🚀

---
+++ src/components/Pictures.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import LikeButton from './LikeButton';
+
+const apiKey = process.env.REACT_APP_NASA_API_KEY;
+
+class Pictures extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount(){
+    fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-7-28&api_key=${apiKey}`)
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Error fetching the pictures")
+        }
+        return response.json()
+      .then(data => {
+        this.setState({data: data});
+      })
+      .catch(err => {
+        throw Error(err.message);
+      });
+    });
+  }
+
+  render(){
+
+    return (
+      <div>
+
+        <p>"Curiosity Rover Pictures Taken On July 28th 2021"</p>
+        {this.state.data.photos && this.state.data.photos.map(picture =>
+          <article key={picture.id}>
+            <img
+              src={picture.img_src}
+              alt="N/A"
+
+            />
+
+            <header> {picture.rover.name} - {picture.camera.full_name} </header>
+            <h4> {picture.earth_date} </h4>
+            <LikeButton/>
+          </ article>)}
+
+      </div>
+    )
+  }
+}
+
+export default Pictures;
